7d976cf7d55008e44e1d21bd539985b683d72233
Harada Taiyo <jeansibelius1865128@gmail.com>
2025-02-04 20:35:55 +0900

    fix: ensure input-div class is always applied

--- src/InputDiv.tsx
+++ src/InputDiv.tsx
@@ -1,39 +1,46 @@
 import { ReactNode, HTMLProps, useEffect } from "react";
 import { useInputDivContext } from "./InputDivProvider";
 import validate from "./validation";
-import './style.css';
-
+import "./style.css";
 
 type InputDivProps = {
-  children?: ReactNode,
-  label?: string,
-  inputKey: string | string[],
-  isEditing?: boolean,
-  placeholder?: string,
-  defaultValue?: string,
-  parser?: (text: string) => any,
+  children?: ReactNode;
+  label?: string;
+  inputKey: string | string[];
+  isEditing?: boolean;
+  placeholder?: string;
+  defaultValue?: string;
+  parser?: (text: string) => any;
 } & HTMLProps<HTMLDivElement>;
 
-export const InputDiv = ({ children, label: propLabel, inputKey, isEditing=true, placeholder='', defaultValue='', parser, ...props }: InputDivProps) => {
-  const inputKeyStr = Array.isArray(inputKey) ? inputKey.join('.') : inputKey;
+export const InputDiv = ({
+  children,
+  label: propLabel,
+  inputKey,
+  isEditing = true,
+  placeholder = "",
+  defaultValue = "",
+  parser,
+  ...props
+}: InputDivProps) => {
+  const inputKeyStr = Array.isArray(inputKey) ? inputKey.join(".") : inputKey;
 
-  const { registerParser, providerLabel, providerIsEditing } = useInputDivContext();
+  const { registerParser, providerLabel, providerIsEditing } =
+    useInputDivContext();
   const label = validate.label(providerLabel, propLabel);
-  
-  useEffect(() => parser && registerParser(label, inputKeyStr, parser), [])
+
+  useEffect(() => parser && registerParser(label, inputKeyStr, parser), []);
 
   const editProps = {
-    className: `${props.className} input-div`,
+    ...props,
+    className: `input-div ${props.className}`,
     contentEditable: providerIsEditing || isEditing,
     suppressContentEditableWarning: true,
     spellCheck: false,
     placeholder: placeholder,
-    'data-label': label,
-    'data-input-key': inputKeyStr,
-    ...props
-  }
+    "data-label": label,
+    "data-input-key": inputKeyStr,
+  };
 
-  return (
-    <div {...editProps}>{defaultValue || children}</div>
-  );
+  return <div {...editProps}>{defaultValue || children}</div>;
 };
